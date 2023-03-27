924403699bdd0df12bdfde1f11a0f3f7f47dbc57
Pareisago Komodo <pareisago@gmail.com>
2023-03-26 23:30:30 -0400

    Add names to ID script

---
+++ 2-network-generation/names_dictionary.py
@@ -0,0 +1,19 @@
+import fileinput
+
+file = fileinput.input("./2-network-generation/directors.csv")
+line = file.readline()
+
+def get_dict_from_csv():
+    director_to_id = {}
+    id_to_director = {}
+
+    for line in file:
+        line = line.strip().split(",")
+        # make names by last-first and fill spaces with single dash
+        name_str = str(line[0] + "-" + line[1]).replace(" ", "-").replace("--", "-")
+        # get id by splitting the url
+        id_str = line[-1].split("/")[4]
+        director_to_id[name_str] = id_str
+        id_to_director[id_str] = name_str
+
+    return director_to_id
