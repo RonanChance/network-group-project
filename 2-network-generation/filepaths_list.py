230766b892042a4a5cb7dc077f265faf3f16a8cd
Pareisago Komodo <pareisago@gmail.com>
2023-04-12 13:18:03 -0400

    Update network generation

--- 2-network-generation/filepaths_list.py
+++ 2-network-generation/filepaths_list.py
@@ -1,7 +1,7 @@
 import os
 
+# Recursively search through the directory and all its subdirectories
 def find_file_paths():
-    # Recursively search through the directory and all its subdirectories
     i = 0
     film_directors_paths = []
     for root, dirs, files in os.walk("../1-data-extraction/"):
@@ -10,5 +10,4 @@ def find_file_paths():
             if file.endswith(".jsonl"):
                 film_directors_paths.append(os.path.join(root, file))
                 i += 1
-    # print("Found", i, "director files.")
     return film_directors_paths
--- 2-network-generation/network_gen.py
+++ 2-network-generation/network_gen.py
@@ -4,6 +4,8 @@ import collections
 import fileinput
 import json
 
+# recursive function that iterates the json object and extracts the names
+# will be adjusting later to get generalized categories (currently getting specific credits)
 def extract_names(json_obj, names_set, credits_dict):
     if isinstance(json_obj, dict):
         for key, value in json_obj.items():
@@ -21,6 +23,8 @@ def extract_names(json_obj, names_set, credits_dict):
         for item in json_obj:
             extract_names(item, names_set, credits_dict)
 
+# iterates through a jsonl file and returns a dictionary of names and their count
+# repetitions of a name in the same line of a json are ignored (same person in different roles in the same film is a count of 1)
 def process_jsonl(path):
     i = 0
     # this is a type of dictionary that has a default value of 1
@@ -39,6 +43,8 @@ def process_jsonl(path):
     # return the sorted dictionary for this director
     return sorted(names_dict.items(), key=lambda x:x[1]), credits_dict
 
+# function that converts the crew names (and their count), director name, and credit dictionaries to a graph
+# the count of the names is the weights of the edges, and credit is put in the attribute dictionary
 def list_to_graph(G, names_list, director_name, credits_dict):
     for name, weight in names_list:
         G.add_edge(director_name, name, weight=weight)
@@ -49,6 +55,11 @@ def list_to_graph(G, names_list, director_name, credits_dict):
     return G
 
 if __name__ == "__main__":
+    '''
+    This script will generate a network of directors and their crew members.
+    It will be a directed and weighted network, saved as a gephi file.
+    '''
+
     # establish a directed graph
     G = nx.DiGraph()
 
@@ -59,10 +70,6 @@ if __name__ == "__main__":
         processed_data, credits_dict = process_jsonl(path)
         director_name = path.split("/")[-1].replace("-", " ").replace(".jsonl", "")
         list_to_graph(G, processed_data, director_name, credits_dict)
-        print(processed_data)
-    
-    for node in G.nodes():
-        print(node, "hi")
     
     for source, target, data in G.edges(data=True):
         print(f"{source} -> {target}, weight: {data['weight']}")
