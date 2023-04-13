f21c800910e282c1e8db543fc320ea3fc8674688
Pareisago Komodo <pareisago@gmail.com>
2023-04-12 23:13:24 -0400

    Store role types in networkx graph

--- 2-network-generation/filepaths_list.py
+++ 2-network-generation/filepaths_list.py
@@ -1,6 +1,8 @@
+import fileinput
 import os
+import json
 
-# Recursively search through the directory and all its subdirectories
+# Recursively search through the directory and all subdirectories
 def find_file_paths():
     i = 0
     film_directors_paths = []
@@ -11,3 +13,27 @@ def find_file_paths():
                 film_directors_paths.append(os.path.join(root, file))
                 i += 1
     return film_directors_paths
+
+# get list of all possible roles across all jsonl files
+def find_role_names():
+    file_paths = sorted(find_file_paths())
+    gen_title_set = set()
+
+    for file in file_paths:
+        file = fileinput.input(file)
+        for line in file:
+            data = json.loads(line)
+
+            for i in range(len(data["full_credits"])):
+                gen_title = data["full_credits"][i]['role']
+                gen_title_set.add(gen_title)
+        
+        file.close()
+
+    return list(gen_title_set)
+
+
+# if __name__ == "__main__":
+#     gen_title_set = find_role_names()
+#     print(gen_title_set)
+#     print(len(gen_title_set))
--- 2-network-generation/network_gen.py
+++ 2-network-generation/network_gen.py
@@ -1,56 +1,73 @@
-from filepaths_list import find_file_paths
+from filepaths_list import find_file_paths, find_role_names
 import networkx as nx
 import collections
 import fileinput
 import json
 
-# recursive function that iterates the json object and extracts the names
-# will be adjusting later to get generalized categories (currently getting specific credits)
-def extract_names(json_obj, names_set, credits_dict):
-    if isinstance(json_obj, dict):
-        for key, value in json_obj.items():
-            if key == 'name':
-                # keep track of the names
-                names_set.add(value)
+# Get names and information about every individual in json file
+def extract_info(data):
+    credits_dict = collections.defaultdict(set)
+    names_set = set()
 
-                # also access their respective credits
-                for k, v in json_obj.items():
-                    if k == 'credit':
-                        credits_dict[value].add(v)
-            else:
-                extract_names(value, names_set, credits_dict)
-    elif isinstance(json_obj, list):
-        for item in json_obj:
-            extract_names(item, names_set, credits_dict)
+    i = 0
+    for i in range(len(data["full_credits"])):
+        gen_title = data["full_credits"][i]['role']
+        
+        for j in range(len(data["full_credits"][i]['crew'])):
+            person = data["full_credits"][i]['crew'][j]['name']
+            credits_dict[person].add(gen_title)
+            names_set.add(person)
+        
+        i += 1
+    return credits_dict, names_set
 
 # iterates through a jsonl file and returns a dictionary of names and their count
 # repetitions of a name in the same line of a json are ignored (same person in different roles in the same film is a count of 1)
 def process_jsonl(path):
-    i = 0
     # this is a type of dictionary that has a default value of 1
-    names_dict = collections.defaultdict(int)
+    name_weights = collections.defaultdict(int)
     credits_dict = collections.defaultdict(set)
     
     for line in fileinput.input(path):
         # load the data in json object
         data = json.loads(line)
-        names_set = set()
-        # add all the new names to our running set
-        extract_names(data, names_set, credits_dict)
-        for item in names_set:
-            names_dict[item] += 1
-    
+        single_credits_dict, single_names_set = extract_info(data)
+        
+        # increment weights, combine credits
+        for name in single_names_set:
+            name_weights[name] += 1
+        
+        for key, value in single_credits_dict.items():
+            if key in credits_dict:
+                credits_dict[key] = credits_dict[key].union(value)
+            else:
+                credits_dict[key] = value
+        
     # return the sorted dictionary for this director
-    return sorted(names_dict.items(), key=lambda x:x[1]), credits_dict
+    return name_weights, credits_dict
 
 # function that converts the crew names (and their count), director name, and credit dictionaries to a graph
 # the count of the names is the weights of the edges, and credit is put in the attribute dictionary
 def list_to_graph(G, names_list, director_name, credits_dict):
-    for name, weight in names_list:
+    for name, weight in names_list.items():
         G.add_edge(director_name, name, weight=weight)
 
-    for node in G.nodes():
-        G.add_node(node, attr_dict={"credit": list(credits_dict[node])})
+    # for name, cred in credits_dict.items():
+    #     credits_dict[name] = list(cred)
+
+    # establish 0 on every role type for each node
+    role_names = find_role_names()
+    for cat in role_names:
+        if len(G.nodes[name]) < len(role_names):
+            for name in names_list:
+                G.nodes[name][cat] = 0
+        # also assign zeroes for director if they haven't been assigned roles yet
+        if len(G.nodes[director_name]) < len(role_names):
+            G.nodes[director_name][cat] = 0
+    
+    for key, cred_list in credits_dict.items():
+        for credit in cred_list:
+            G.nodes[key][credit] = 1
 
     return G
 
@@ -66,13 +83,15 @@ if __name__ == "__main__":
     # sort for our sake
     film_directors_paths = sorted(find_file_paths())
     
+    credits_dict = {}
     for path in film_directors_paths[:1]:
-        processed_data, credits_dict = process_jsonl(path)
+        name_weights, credits_dict_director = process_jsonl(path)
         director_name = path.split("/")[-1].replace("-", " ").replace(".jsonl", "")
-        list_to_graph(G, processed_data, director_name, credits_dict)
+
+        list_to_graph(G, name_weights, director_name, credits_dict_director)
     
     for source, target, data in G.edges(data=True):
-        print(f"{source} -> {target}, weight: {data['weight']}")
+        print(source, target, data["weight"])
 
     print("Writing to gephi file...")
     nx.write_gexf(G, "network.gexf")
